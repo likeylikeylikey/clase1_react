af7f4edbb241175a42dca553d2e7047fd4fc26dc
likeylikeylikey <112669263+likeylikeylikey@users.noreply.github.com>
2022-09-01 21:18:08 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ¡Hola! <code>Mi primera</code> app con React
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ¡Segunda clase!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
